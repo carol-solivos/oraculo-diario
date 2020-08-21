c4f9c6b02587cf4ca88994d5fe8acc69e0761fdd
carol <carolmaria.sepulvedaolivos@cencosud.cl>
2020-08-21 19:51:42 -0400

    se llama a api frases, se inserta script google

---
+++ README.md
@@ -0,0 +1,2 @@
+# oraculo
+Una enseñanza para cada día... Generador aleatorio de frases que calan en el subconsciente.
---
+++ app.js
@@ -0,0 +1,31 @@
+
+class Translate {
+	constructor() {
+	}
+}
+let container = document.getElementById("container");
+
+let api = async () => {
+	let data = await fetch("https://type.fit/api/quotes").then(x => {
+		return x.json();
+	})
+	let random = Math.floor(Math.random() * data.length);
+	container.innerHTML = `<h1>${data[random].text}</h1>
+												<h4>${data[random].author || "Anónimo"}</h4>`
+
+												translate();
+	// // document.getElementsByClassName("goog-te-combo")[0].value = "su"
+}
+
+
+document.getElementById("ask").addEventListener("click", api);
+
+
+let translate = async () => {
+	let translate = await new google.translate.TranslateElement(
+		{
+			pageLanguage: 'en',
+			includedLanguages: 'es',
+			layout: google.translate.TranslateElement.InlineLayout.SIMPLE
+		}, 'translate');
+	}
