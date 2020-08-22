07686ca55b2eece7a88a025ca3a82e4398c9bc98
carol-solivos <carol.solivos@gmail.com>
2020-08-21 22:18:45 -0400

    ¡se agrega animaciones a button, luz y scale

--- app.js
+++ app.js
@@ -9,11 +9,18 @@ let api = async () => {
 	let data = await fetch("https://type.fit/api/quotes").then(x => {
 		return x.json();
 	})
-	let random = Math.floor(Math.random() * data.length);
-	container.innerHTML = `<h1>${data[random].text}</h1>
-												<h4>${data[random].author || "Anónimo"}</h4>`
+	let btn = document.getElementById("ask");
+	btn.classList.add("scale");
+	await setTimeout(() => btn.style.display = "none", 600)
+	setTimeout(() => {
+		let random = Math.floor(Math.random() * data.length);
+		container.innerHTML = `<h1>"${data[random].text}"</h1>
+													<h4>${data[random].author || "Anónimo"}</h4>`
+	}, 1000)
 
-												translate();
+
+
+	translate();
 	// // document.getElementsByClassName("goog-te-combo")[0].value = "su"
 }
 
